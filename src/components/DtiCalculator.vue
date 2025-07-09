5930974964cdc37fec264bda6aaa7772c9f382cb
GuillaumeFerron <guillaume.ferron2@gmail.com>
2025-07-09 13:57:27 +0200

    Addition of Tonik and Tendo CL

--- src/components/DtiCalculator.vue
+++ src/components/DtiCalculator.vue
@@ -18,9 +18,17 @@
           <label for="net-income">Net Income</label>
           <input type="number" class="form-control" step=".01" id="net-income" v-model="data.netIncome">
         </div>
+        <div class="mt-3">
+          <label for="tendo-cl">Tendo CL</label>
+          <input type="number" class="form-control" step="0.01" id="tendo-cl" v-model="data.tendoCl">
+        </div>
       </div>
       <div class="col-12 col-lg-6">
         <div>
+          <label for="tonik-cl">Tonik CL</label>
+          <input type="number" class="form-control" step="0.01" id="tonik-cl" v-model="data.tonikCl">
+        </div>
+        <div class="mt-3">
           <label for="schedule">Schedule (copied from user's account)</label>
           <textarea class="form-control" id="schedule" v-model="data.schedule" rows="4"></textarea>
         </div>
@@ -32,10 +40,36 @@
     </div>
     <hr class="mt-4">
     <div class="row mt-3">
+      <h5>Credit Capacity</h5>
+      <div>
+        <ul>
+          <li>Max CL (Minimum between Tonik and Tendo CL): {{ actualCl }}</li>
+          <li>Outstanding balance: {{ totalDue }}</li>
+          <li>Available amount as of today: {{ currentAvailable }}</li>
+        </ul>
+      </div>
+    </div>
+    <hr class="mt-4">
+    <div class="row mt-3">
       <h5>Schedule</h5>
-      <small><i>The highlighted cells will show the first payment for which the available amount is 50% of the total
-          amount
-          the customer could avail if no pending payment (PHP{{ maxPayment }}). If none is highlighted, it means that the user will have to repay their last payment in order to avail a new loan.</i></small>
+      <small>
+        <i>
+          <ul>
+            <li>
+              The highlighted cells will show the first payment for which, the available amount is 50% of the total amount the customer could avail if no pending payment (PHP{{ maxPayment }}).
+            </li>
+            <li>
+              If none is highlighted, the user will have to repay their last payment in order to avail a new loan.
+            </li>
+            <li>
+              The available amount takes into account the Tonik CL. If the Tendo CL based available amount were to go over the Tonik CL, then the value is capped at the latter.
+            </li>
+            <li>
+              Note that these calculations take into account the full amount from the installments - this allows for some buffer for user experience, but on paper should only be taking the principal into account.
+            </li>
+          </ul>
+        </i>
+      </small>
       <div class="my-2">
         <table class="table table-striped table-sm" id="schedule-table">
           <thead>
@@ -50,6 +84,7 @@
               <td>{{ row[2] }}</td>
               <td>{{ row[3] }}</td>
               <td>{{ row[4] }}</td>
+              <td>{{ row[5] }}</td>
             </tr>
           </tbody>
         </table>
@@ -80,7 +115,9 @@
           dti: 30,
           payroll: 2,
           netIncome: null,
-          schedule: null
+          schedule: null,
+          tendoCl: null,
+          tonikCl: null
         },
         cacheProcessed: false,
         headers: HEADERS,
@@ -108,10 +145,29 @@
         .map(_ => _.replace(/((₱ )|(,))/g, '').split(/\t/g))
       },
       scheduleWithAvailable () {
-        return this.computedSchedule.map(_ => ([..._, Math.max(Math.ceil(((this.data.dti / 100) * this.data.netIncome / this.data.payroll - _[3]) * 100) / 100, 0)]))
+        let cumulative = 0
+        return this.computedSchedule.map(_ => {
+          const amount = Math.max(Math.ceil(((this.data.dti / 100) * this.data.netIncome / this.data.payroll - _[3]) * 100) / 100, 0)
+          cumulative += amount
+          return [..._,
+            Math.min(cumulative, this.actualCl)
+          ]
+        })
       },
       maxPayment() {
-        return Math.ceil((this.data.dti / 100) * this.data.netIncome / this.data.payroll * 100) / 100
+        return Math.min(this.scheduleWithAvailable.reduce((acc, _) => acc >= _[4] ? acc : _[4], 0), this.actualCl)
+      },
+      actualCl() {
+        return Math.min(this.data.tendoCl, this.data.tonikCl)
+      },
+      totalDue() {
+        return this.scheduleWithAvailable.reduce((acc, _) => acc + parseFloat(_[3]), 0)
+      },
+      currentAvailable() {
+        if (!this.scheduleWithAvailable || !this.scheduleWithAvailable[0]) {
+          return 0
+        }
+        return Math.round(Math.min(Math.max(this.scheduleWithAvailable[0][4] - this.scheduleWithAvailable[0][3], 0), this.actualCl) * 100) / 100
       }
     },
     methods: {
